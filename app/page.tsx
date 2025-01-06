7d0b61a7ebb97f1a33ec644f53d5c975dda3f162
BEWILDERYAZ\dhias <dhiasmnaufal0@mail.ugm.ac.id>
2025-01-06 09:37:46 +0700

    Uppdate Page

--- app/page.tsx
+++ app/page.tsx
@@ -77,8 +77,5 @@ export default function Home() {
         </div>
       </div>
     </div >
-    // <div>
-    //  
-    // </div>
   )
 }
--- components/MapCesium.tsx
+++ components/MapCesium.tsx
@@ -6,14 +6,15 @@ import {
     Cartesian3,
     IonResource,
     OpenStreetMapImageryProvider,
+    UrlTemplateImageryProvider,
 } from 'cesium'
-import { useContext, useRef } from 'react'
+import { useContext, useRef, useState } from 'react'
 import { ViewerContext } from '@/context/ViewerContext'
 Ion.defaultAccessToken = process.env.NEXT_PUBLIC_CESIUM_ACCESS_TOKEN as string
 
 const CesiumViewer = () => {
-    const osmImageryProvider = new OpenStreetMapImageryProvider({
-        url: "https://a.tile.openstreetmap.org/",
+    const osmImageryProvider = new UrlTemplateImageryProvider({
+        url: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
     });
     const {
         action: { setCesiumCamera },
@@ -23,24 +24,34 @@ const CesiumViewer = () => {
         if (ref.current?.cesiumElement) {
             const viewer = ref.current.cesiumElement;
             const terrainProviderViewModels = viewer.baseLayerPicker.viewModel.terrainProviderViewModels;
+            const imageryProviderViewModels = viewer.baseLayerPicker.viewModel.imageryProviderViewModels;
+
             const desiredTerrain = terrainProviderViewModels.find(model => model.name === "Cesium World Terrain");
             if (desiredTerrain) {
                 viewer.baseLayerPicker.viewModel.selectedTerrain = desiredTerrain
+
             }
+            viewer.imageryLayers.addImageryProvider(osmImageryProvider)
             setCesiumCamera(ref.current.cesiumElement.camera)
         }
     }
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    const handleTilesetLoad = () => {
+        console.log("first")
+    };
     return (
-        <Viewer ref={ref} full timeline={false} animation={false} >
+        <Viewer ref={ref} full timeline={false} animation={false}  >
             <CameraFlyTo
-                destination={Cartesian3.fromDegrees(116.708881, - 0.972826, 1200)}
+                destination={Cartesian3.fromDegrees(110.376100, -7.790165, 1000)}
                 orientation={{ heading: CesiumMath.toRadians(15.0), pitch: CesiumMath.toRadians(-35.0), roll: 0.0 }}
                 onComplete={setTerrain}
                 duration={0}
                 once
             />
-            <Cesium3DTileset url={IonResource.fromAssetId(96188)} />
-            <ImageryLayer imageryProvider={osmImageryProvider} />
+            {/* <ImageryLayer imageryProvider={osmImageryProvider} /> */}
+            <Cesium3DTileset url={IonResource.fromAssetId(96188)} onReady={handleTilesetLoad} />
         </Viewer>
     )
 }
